7a13ce3cc07abb029c0e4ff6d6cdd5160d87e865
Riograndes <46006195+Riograndes@users.noreply.github.com>
2020-06-09 15:26:17 +0300

    Update lesson4.6.py

--- lesson4.6.py
+++ lesson4.6.py
@@ -2,11 +2,3 @@ import itertools
 
 for i in itertools.count(int(input('Введите стартовое число '))):
     print(i)
-b = a()
-x = 0
-for i in b:
-    if x < 15:
-        print(i)
-        x += 1
-    else:
-        break
