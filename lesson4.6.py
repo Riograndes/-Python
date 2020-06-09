01ce6603a45d151caec07dac6633decde90dc4bd
Riograndes <46006195+Riograndes@users.noreply.github.com>
2020-06-09 15:29:20 +0300

    Update lesson4.6.py

--- lesson4.6.py
+++ lesson4.6.py
@@ -2,3 +2,10 @@ import itertools
 
 for i in itertools.count(int(input('Введите стартовое число '))):
     print(i)
+
+    
+#import itertools
+#
+#list = [1, 'A', 2, None, "B", "C"]
+#for i in itertools.cycle(list):
+#    print(i)
